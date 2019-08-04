1d0e5187aa63b2eea4d55d1f4e521b1f5ee0ac00
aisav <artur.isaverdyan@gmail.com>
2019-08-04 10:15:26 +0400

    gatsby-transformer-remark

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,41 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 
 import Layout from "../components/layout"
 // export default () => <div>Hello world!</div>
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title,
+              date
+            }
+            html
+            excerpt
+            timeToRead
+    
+          }
+        }
+      }
+    }
+`)
+  // console.log(data)
   return (
     <Layout>
       <h1>Blog</h1>
-      <h2>Posts will be shown here</h2>
+      <ol>
+      {data.allMarkdownRemark.edges.map((e, i) => {
+          return (<li key={i}>
+            <h2>{e.node.frontmatter.title}</h2>
+            <p key={i}>{e.node.frontmatter.date}</p>
+          </li>)
+        })
+      }
+      </ol>
     </Layout>
   )
 }
